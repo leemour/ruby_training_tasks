41213ca08dcca80481416821b7c2d794d9f2310a
Viacheslav Ptsarev <leemour@gmail.com>
2019-07-07 10:18:52 +0300

    Improve Command a bit

--- mortal_combat/lib/mortal_combat/command.rb
+++ mortal_combat/lib/mortal_combat/command.rb
@@ -2,18 +2,24 @@
 
 module MortalCombat
   class Command
-    NEW_GAMES     = %w[yes no].freeze
-    FIRST_MOVES   = %w[player monster].freeze
-    ATTACK_TYPES  = %w[normal special magic].freeze
+    VALID_INPUT = {
+      "new_game"    => %w[yes no],
+      "first_move"  => %w[player monster],
+      "attack_type" => %w[normal special magic]
+    }.freeze
     PROMPTS = {
       'new_game'    => "Do you want to start new game? #{NEW_GAMES}",
       'first_move'  => "Choose who moves first: #{FIRST_MOVES}",
       'attack_type' => "Choose attack type: #{ATTACK_TYPES}",
+      'exit'        => "Bye-bye!"
     }.freeze
 
     attr_reader :command
 
     def self.gets(command_name)
+      unless VALID_INPUT.keys.include? command_name
+        raise InvalidCommand, "Command #{command_name} doesn't exist"
+      end
       puts PROMPTS[command_name]
       print ">> "
       command = new Kernel.gets.strip.downcase
@@ -23,19 +29,18 @@ module MortalCombat
     def initialize(command)
       @command = command
       if command == 'quit'
-        puts "Bye bye!"
+        puts Command::PROMPTS['exit']
         exit
       end
     end
 
     def gets_until_valid(command_name)
-      valid_commands = Command.const_get("#{command_name}s".upcase)
+      valid_commands = Command::VALID_INPUT[command_name]
       unless valid_commands.include? command
-        puts "Invalid command. Valid commands are: #{valid_commands}"
+        puts "Invalid command #{command}. Valid commands are: #{valid_commands}"
         return Command.gets(command_name)
       end
       command
     end
   end
 end
-
